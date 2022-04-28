15a14e9ee6a19149d34a4b5d934acd67092e4917
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-28 19:32:19 +0530

    [Madhavee] Add method to load IndianStateCode File successfully with test cases

--- IndianCensusTesting/CensusDataTesting.cs
+++ IndianCensusTesting/CensusDataTesting.cs
@@ -100,5 +100,87 @@ namespace IndianCensusTesting
                 Assert.AreEqual(excepted, ex.Message);
             }
         }
+        /// <summary>
+        /// TC 2.1 - Check to ensure the Number of Record matches with IndianStateCoe CSV File
+        /// </summary>
+        [TestMethod]
+        public void GivenIndianStateCodesCSVShouldReturnsRecords()
+        {
+            string path = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\CSVFiles\IndiaStateCode.csv";
+            stateRecords = adapter.LoadCsvData(CensusAnalyser.Country.INDIA, path, "SrNo,State Name,TIN,StateCode");
+            Assert.AreEqual(37, stateRecords.Count);
+        }
+        /// <summary>
+        /// TC 2.2 - Given the Indian State Code if incorrect path Returns a custom Exception
+        /// </summary>
+        [TestMethod]
+        public void GivenWrongFilePathOfIndianStateCodeReturnsCustomException()
+        {
+            string path = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\CSVFiles\Indian.csv";
+            string excepted = "File Not Found";
+            try
+            {
+                stateRecords = adapter.LoadCsvData(CensusAnalyser.Country.INDIA, path, "SrNo,State Name,TIN,StateCode");
+                Assert.AreEqual(37, stateRecords.Count);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(excepted, ex.Message);
+            }
+        }
+        /// <summary>
+        /// TC 2.3 - Given the Indian State Code CSV File if incorrect file type Returns a custom Exception
+        /// </summary>
+        [TestMethod]
+        public void GivenWrongFileTypeOfIndianStateCodeReturnsCustomException()
+        {
+            string path = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\CSVFiles\IndiaStateCode.txt";
+            string excepted = "Invalid file type";
+            try
+            {
+                stateRecords = adapter.LoadCsvData(CensusAnalyser.Country.INDIA, path, "SrNo,State Name,TIN,StateCode");
+                Assert.AreEqual(37, stateRecords.Count);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(excepted, ex.Message);
+            }
+        }
+        /// <summary>
+        /// TC 2.4 - Given the Indian State Code CSV File when correct but delimiter incorrect returns a custom exception
+        /// </summary>
+        [TestMethod]
+        public void GivenWrongDelimiterOfIndianStateCodeReturnsCustomException()
+        {
+            string path = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\CSVFiles\DelimiterIndiaStateCode.csv";
+            string excepted = "File Containers Wrong Delimiter";
+            try
+            {
+                stateRecords = adapter.LoadCsvData(CensusAnalyser.Country.INDIA, path, "SrNo,State Name,TIN,StateCode");
+                Assert.AreEqual(37, stateRecords.Count);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(excepted, ex.Message);
+            }
+        }
+        /// <summary>
+        /// TC 2.5 - Given the Indian State Code CSV File when correct but incorrect dataheader returns a custom exception
+        /// </summary>
+        [TestMethod]
+        public void GivenWrongDataHeaderOfIndianStateCodeReturnsCustomException()
+        {
+            string path = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\CSVFiles\WrongIndiaStateCode.csv";
+            string excepted = "Incorrect header in Data";
+            try
+            {
+                stateRecords = adapter.LoadCsvData(CensusAnalyser.Country.INDIA, path, "SrNo,State Name,TIN,StateCode");
+                Assert.AreEqual(37, stateRecords.Count);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(excepted, ex.Message);
+            }
+        }
     }
 }
